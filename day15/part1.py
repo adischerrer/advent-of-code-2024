fa4c8eeba7e5fdf4970c2094df00f1083f465d26
Adi <adi.scherrer@gmail.com>
2024-12-31 01:06:33 +0100

    Add solution for day15 (part1)

--- day15/part1.py
+++ day15/part1.py
@@ -1,7 +1,7 @@
 import time
 from typing import Final
 
-INPUT_FILE: Final[str] = 'input_example1.txt'
+INPUT_FILE: Final[str] = 'input.txt'
 
 
 class Pos:
@@ -12,11 +12,11 @@ class Pos:
 
 class Maze:
     def __init__(self, lines: list[str]) -> None:
-        size_y: int = len(lines)
-        size_x: int = len(lines[0])
-        self.pos: list[list[Pos]] = [[Pos() for _ in range(size_x)] for _ in range(size_y)]
-        for y in range(size_y):
-            for x in range(size_x):
+        self.size_y: int = len(lines)
+        self.size_x: int = len(lines[0])
+        self.pos: list[list[Pos]] = [[Pos() for _ in range(self.size_x)] for _ in range(self.size_y)]
+        for y in range(self.size_y):
+            for x in range(self.size_x):
                 if lines[y][x] == '#':
                     self.get_pos(x, y).is_wall = True
                 elif lines[y][x] == 'O':
@@ -60,13 +60,70 @@ def print_maze(maze: Maze) -> None:
         print()
 
 
+def get_direction(char: str) -> tuple[int, int]:
+    match char:
+        case '>':
+            return 1, 0
+        case 'v':
+            return 0, 1
+        case '<':
+            return -1, 0
+        case _:
+            return 0, -1
+
+
+def find_space(maze: Maze, direction_char: str) -> tuple[int, int] | None:
+    direction: tuple[int, int] = get_direction(direction_char)
+    step: int = 0
+    while True:
+        step += 1
+        check_pos = maze.robot_pos[0] + step * direction[0], maze.robot_pos[1] + step * direction[1]
+        if maze.get_pos(check_pos[0], check_pos[1]).is_wall:
+            return None
+        elif not maze.get_pos(check_pos[0], check_pos[1]).is_box:
+            return check_pos
+
+
+def move_boxes(maze: Maze, space_pos: tuple[int, int]) -> None:
+    # Move all boxes between robot and space_pos towards space_pos
+    if not space_pos[0] == maze.robot_pos[0]:
+        x_dir: int = 1 if space_pos[0] < maze.robot_pos[0] else -1
+        y: int = space_pos[1]
+        for x in range(space_pos[0], maze.robot_pos[0], x_dir):
+            if maze.get_pos(x + x_dir, y).is_box:
+                maze.get_pos(x + x_dir, y).is_box = False
+                maze.get_pos(x, y).is_box = True
+            if maze.robot_pos == (x + x_dir, y):
+                maze.robot_pos = (x, y)
+    if not space_pos[1] == maze.robot_pos[1]:
+        y_dir: int = 1 if space_pos[1] < maze.robot_pos[1] else -1
+        x: int = space_pos[0]
+        for y in range(space_pos[1], maze.robot_pos[1], y_dir):
+            if maze.get_pos(x, y + y_dir).is_box:
+                maze.get_pos(x, y + y_dir).is_box = False
+                maze.get_pos(x, y).is_box = True
+            if maze.robot_pos == (x, y + y_dir):
+                maze.robot_pos = (x, y)
+
+
 def calc_coord(maze: Maze) -> int:
-    return 0
+    coord_total = 0
+    for x in range(maze.size_x):
+        for y in range(maze.size_y):
+            if maze.get_pos(x, y).is_box:
+                coord_total += x + 100 * y
+    return coord_total
 
 
 def main():
     maze: Maze = read_input_maze()
+    moves: str = read_input_moves()
     print(f'Moves: {read_input_moves()}')
+    print_maze(maze)
+    for move in moves:
+        space_pos = find_space(maze, move)
+        if space_pos is not None:
+            move_boxes(maze, space_pos)
     print_maze(maze)
     print(f'GPS coordinates: {calc_coord(maze)}')
 
